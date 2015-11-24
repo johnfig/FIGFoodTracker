33df44332a7a6c1260f46801d5859cc6c3d56f8f
John Figueiredo <john@johnfigueiredo.com>
2015-11-23 22:57:19 -0800

    Add UITextFieldDelegate to view controller

--- FIGFoodTracker/ViewController.swift
+++ FIGFoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
     
     // MARK: Actions
@@ -26,7 +26,6 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
